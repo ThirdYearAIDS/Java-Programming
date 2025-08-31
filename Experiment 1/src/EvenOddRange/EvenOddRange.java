2ddc1e3de96d0afb7a1e121728991f91ebc9c3be
MixMasterMatrix <nicketushah12@gmail.com>
2025-08-31 12:09:56 +0530

    add expt 1

---
+++ Experiment 1/src/EvenOddRange/EvenOddRange.java
@@ -0,0 +1,9 @@
+package EvenOddRange;
+
+public class EvenOddRange {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 30; i++) {
+            System.out.println(i % 2 == 0 ? "Even" : "Odd");
+        }
+    }
+}
---
+++ Experiment 1/src/EvenOrOdd/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package EvenOrOdd;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any number: ");
+        int num = sc.nextInt();
+
+        System.out.println(num % 2 == 0 ? "Even" : "Odd");
+
+        sc.close();
+    }
+}
